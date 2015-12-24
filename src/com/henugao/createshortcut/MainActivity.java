631d7fedbb6370023c51c3fdc287622119306d99
Gao Yuejin <henugao@example.com>
2015-12-24 21:59:12 +0800

    实现了创建快捷方式的功能

--- src/com/henugao/createshortcut/MainActivity.java
+++ src/com/henugao/createshortcut/MainActivity.java
@@ -1,8 +1,11 @@
 package com.henugao.createshortcut;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -12,11 +15,29 @@ public class MainActivity extends Activity {
 		setContentView(R.layout.activity_main);
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
+	public void createShortcut(View view){
+		Intent intent = new Intent();
+		intent.setAction("com.android.launcher.action.INSTALL_SHORTCUT");
+		
+		/**
+		 * 1 你想干什么事情
+		 * 2 叫什么名字
+		 * 3 长什么样子
+		 */
+		
+		Intent dowhtIntent = new Intent();
+		//我想打电话
+		dowhtIntent.setAction(Intent.ACTION_CALL);
+		dowhtIntent.setData(Uri.parse("tel://110"));
+		intent.putExtra(Intent.EXTRA_SHORTCUT_INTENT, dowhtIntent);
+		//名字叫做什么
+		intent.putExtra(Intent.EXTRA_SHORTCUT_NAME, "打电话");
+		//长什么样子
+		intent.putExtra(Intent.EXTRA_SHORTCUT_ICON_RESOURCE, R.drawable.ic_launcher);
+		
+		sendBroadcast(intent);
+		
+		
 	}
 
 }
